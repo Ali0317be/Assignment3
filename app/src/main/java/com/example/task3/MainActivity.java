8601159e4015f39dc25644f056922332485a6966
Ali <Ali@gmail.com>
2021-11-11 01:20:38 +0500

    Practise

--- app/src/main/java/com/example/task3/MainActivity.java
+++ app/src/main/java/com/example/task3/MainActivity.java
@@ -9,8 +9,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private Button button1;
+    private Button button4;
+    private Button button3;
+
+
 
     public void gotoURl(View view){
         String url = "https://github.com/Ali0317be/Assignment3";
@@ -25,18 +27,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        button = (Button) findViewById(R.id.btn_three);
-        button.setOnClickListener(new View.OnClickListener() {
+        button3 = (Button) findViewById(R.id.btn_three);
+        button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                Intent intent = new Intent(MainActivity.this, MainActivity4.class);
                 startActivity(intent);
             }
 
         });
 
-        button1 = (Button) findViewById(R.id.btn_four);
-        button1.setOnClickListener(new View.OnClickListener() {
+        button4 = (Button) findViewById(R.id.btn_four);
+        button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MainActivity3.class);
---
+++ app/src/main/java/com/example/task3/MainActivity4.java
@@ -0,0 +1,35 @@
+package com.example.task3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    private Button button3;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+
+        button3 = (Button) findViewById(R.id.button6);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity4.this, MainActivity5.class);
+                startActivity(intent);
+            }
+
+        });
+
+    }
+}
